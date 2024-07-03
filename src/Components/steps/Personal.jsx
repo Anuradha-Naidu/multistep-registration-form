28048c7a6ef407b98e97dc33f780205a6fece325
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2024-07-04 02:26:17 +0530

    Update Personal.jsx

--- src/Components/steps/Personal.jsx
+++ src/Components/steps/Personal.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { StepperContext } from '../contexts/StepperContext';
 
-
-const Personal = () => {
+const Personal = ({ onSubmitForm }) => {
   const { userData, setUserData } = useContext(StepperContext);
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: userData
+  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
+  const onSubmit = (data) => {
+    setUserData({ ...userData, ...data });
+    onSubmitForm(data);
   };
 
   return (
-    <div className="flex flex-col">
+    <form id="personalForm" onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
           First Name
@@ -19,13 +22,12 @@ const Personal = () => {
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             type="text"
-            onChange={handleChange}
-            name="firstname"
-            value={userData['firstname'] || ''}
+            {...register('firstname', { required: 'First Name is required' })}
             placeholder="First Name"
-            className="p-1 px-2 apperarance-none outline-none w-full text-gray-800"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
+        {errors.firstname && <p className="text-red-600 text-xs">{errors.firstname.message}</p>}
       </div>
 
       <div className="w-full mx-2 flex-1">
@@ -35,14 +37,14 @@ const Personal = () => {
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             type="text"
-            onChange={handleChange}
-            name="lastname"
-            value={userData['lastname'] || ''}
+            {...register('lastname', { required: 'Last Name is required' })}
             placeholder="Last Name"
-            className="p-1 px-2 apperarance-none outline-none w-full text-gray-800"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
-        </div>   
+        </div>
+        {errors.lastname && <p className="text-red-600 text-xs">{errors.lastname.message}</p>}
       </div>
+
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
           Date Of Birth
@@ -50,16 +52,14 @@ const Personal = () => {
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             type="date"
-            onChange={handleChange}
-            name="date"
-            value={userData['date'] || ''}
+            {...register('date', { required: 'Date of Birth is required' })}
             placeholder="Date Of Birth"
-            className="p-1 px-2 apperarance-none outline-none w-full text-gray-800"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
+        {errors.date && <p className="text-red-600 text-xs">{errors.date.message}</p>}
       </div>
-
-    </div>
+    </form>
   );
 };
 
