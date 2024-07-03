8c182eb642033c8523d9b0e9430bfeae8e3cb052
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2024-07-04 02:27:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Contact from './Components/steps/Contact';
 import Account from './Components/steps/Account';
 import Final from './Components/steps/Final';
 import Summary from './Components/steps/Summary';
+import { useForm, FormProvider } from 'react-hook-form';
 
 const App = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [userData, setUserData] = useState({});
   const [finalData, setFinalData] = useState([]);
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
+  const methods = useForm();
 
   const steps = [
     "Personal Information",
@@ -25,13 +28,13 @@ const App = () => {
   const displayStep = (step) => {
     switch (step) {
       case 1:
-        return <Personal />;
+        return <Personal onSubmitForm={onSubmitForm}/>;
       case 2:
-        return <Contact />;
+        return <Contact onSubmitForm={onSubmitForm}/>;
       case 3:
-        return <Account />;
+        return <Account onSubmitForm={onSubmitForm}/>;
       case 4:
-        return <Summary formData={userData} />; // Pass userData as formData 
+        return <Summary formData={userData} />;
       case 5:
         return <Final />;
       default:
@@ -39,99 +42,121 @@ const App = () => {
     }
   };
 
-  const handleClick = (direction) => {
-    let newStep = currentStep;
-    direction === 'next' ? newStep++ : newStep--;
-    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  const onSubmitForm = async (data) => {
+    setUserData(prevState => ({ ...prevState, ...data }));
+    if (currentStep < steps.length) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handleNextClick = async () => {
+    // Ensure form validation for each step before proceeding
+    const forms = {
+      1: 'personalForm',
+      2: 'contactForm',
+      3: 'accountForm',
+      // Add more forms if needed
+  };
+
+  const formId = forms[currentStep];
+    if (formId) {
+      const formElement = document.getElementById(formId);
+      if (formElement) {
+        const event = new Event('submit', { cancelable: true, bubbles: true });
+        formElement.dispatchEvent(event);
+        if (event.defaultPrevented) {
+          return;
+        }
+      }
+    }
+
+    if (currentStep < steps.length) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
   };
 
   return (
-  <div className='bg-indigo-950'>  
-    <div className='text-center mb-4'>
-      <h1>.</h1>
-      <h1 className='text-4xl font-bold text-white shadow-lg bg-500 py-2 px-4 rounded-lg'>
-        Multi Step Registration Form !</h1>
-    </div>
-    <div className='md:w-3/5 mx-auto shadow-2xl rounded-2xl pb-2 bg-blue-100 flex flex-col'>
-      {/* Stepper */}
-      <div className='container horizontal mt-5'>
-        <Stepper
-          steps={steps}
-          currentStep={currentStep}
-        />
+    <div className='bg-violet-950'>
+      <div className='text-center mb-4'>
+        <h1>.</h1>
+        <h1 className='text-sky-400 text-xl font-semibold font-sans-serif text-center flex-1'>Multi Step Registeration Form </h1>
+            
       </div>
-
-      {/* Display Components */}
-      <div className="my-10 p-10">
-        <StepperContext.Provider value={{
-          userData,
-          setUserData,
-          finalData,
-          setFinalData
-        }}>
-          {displayStep(currentStep)}
-        </StepperContext.Provider>
-      </div>
-
-      {currentStep === steps.indexOf("Summary") + 1 && (
-        <div className="flex flex-col items-center mb-5">
-          <div className="flex items-center my-4">
-            <input
-              type="checkbox"
-              id="terms"
-              checked={isCheckboxChecked}
-              onChange={(e) => setIsCheckboxChecked(e.target.checked)}
-              className="mr-2"
-            />
-            <label htmlFor="terms" className="text-gray-800">
-              I agree to the terms and conditions
-            </label>
-          </div>
-        </div>
-      )}
-
-      {currentStep !== steps.length && (
-        <div className="flex flex-col items-center mb-5">
-          <StepperControl
-            handleClick={handleClick}
-            currentStep={currentStep}
+      <div className='md:w-1/2 mx-auto drop-shadow-2xl rounded-2xl pb-2 bg-violet-50 flex flex-col'>
+        <div className='container horizontal mt-5'>
+          <Stepper
             steps={steps}
-            isCheckboxChecked={isCheckboxChecked}
+            currentStep={currentStep}
           />
         </div>
-      )}
-    </div>
-      <h1>.</h1>
-      <h1>.</h1>
-    <div class="bg-gray-100 py-6">
-    <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div class="flex space-x-4 mb-4 md:mb-0">
-            <a href="#" class="text-gray-600 hover:text-gray-900">About</a>
-            <a href="#" class="text-gray-600 hover:text-gray-900">Blog</a>
-            <a href="#" class="text-gray-600 hover:text-gray-900">Jobs</a>
-            <a href="#" class="text-gray-600 hover:text-gray-900">Press</a>
-            <a href="#" class="text-gray-600 hover:text-gray-900">Accessibility</a>
-            <a href="#" class="text-gray-600 hover:text-gray-900">Partners</a>
+
+        <div className="my-10 p-10">
+          <FormProvider {...methods}>
+            <StepperContext.Provider value={{
+              userData,
+              setUserData,
+              finalData,
+              setFinalData
+            }}>
+              {displayStep(currentStep)}
+            </StepperContext.Provider>
+          </FormProvider>
         </div>
+
+        {currentStep === steps.indexOf("Summary") + 1 && (
+          <div className="flex flex-col items-center mb-5">
+            <div className="flex items-center my-4">
+              <input
+                type="checkbox"
+                id="terms"
+                checked={isCheckboxChecked}
+                onChange={(e) => setIsCheckboxChecked(e.target.checked)}
+                className="mr-2"
+              />
+              <label htmlFor="terms" className="text-gray-800">
+                I agree to the terms and conditions
+              </label>
+            </div>
+          </div>
+        )}
+
+        {currentStep !== steps.length && (
+          <div className="flex flex-col items-center mb-5">
+            <StepperControl
+              handleNextClick={handleNextClick}
+              handlePrevClick={handlePrevClick}
+              currentStep={currentStep}
+              steps={steps}
+              isCheckboxChecked={isCheckboxChecked}
+            />
+          </div>
+        )}
+      </div>
+      <h1>.</h1>
+      <h1>.</h1>
+      <div className="bg-gray-100 py-6">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+          <div className="flex space-x-4 mb-4 md:mb-0">
+            <a href="#" className="text-gray-600 hover:text-gray-900">About</a>
+            <a href="#" className="text-gray-600 hover:text-gray-900">Blog</a>
+            <a href="#" className="text-gray-600 hover:text-gray-900">Jobs</a>
+            <a href="#" className="text-gray-600 hover:text-gray-900">Press</a>
+            <a href="#" className="text-gray-600 hover:text-gray-900">Accessibility</a>
+            <a href="#" className="text-gray-600 hover:text-gray-900">Partners</a>
+          </div>
+        </div>
+        <div className="container mx-auto text-center mt-4">
+          <p className="text-gray-600">©2024 Anuradha Naidu, Inc. All rights reserved</p>
+        </div>
+      </div>
     </div>
-    <div class="container mx-auto text-center mt-4">
-        <p class="text-gray-600">©2024 Anuradha Naidu, Inc. All rights reserved</p>
-    </div>
-    </div>
-      
-    </div>
-  
   );
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
